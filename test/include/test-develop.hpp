93092a628e147f9b6ef09f4ffc3e2f73ba032763
Kartonagnick <Kartonagnick@gmail.com>
2025-11-12 16:40:12 +0300

    upd: test/include/test-develop.hpp (005) PRE

--- test/include/test-develop.hpp
+++ test/include/test-develop.hpp
@@ -1,6 +1,7 @@
 
 // --- Kartonagnick/compile                                   [test-develop.hpp]
-// [2025-11-11][04:30:00] 004 Kartonagnick    
+// [2025-11-12][19:00:00] 005 Kartonagnick PRE
+// [2025-11-11][04:30:00] 004 Kartonagnick
 // [2025-11-10][21:10:00] 003 Kartonagnick
 // [2025-11-06][18:10:00] 002 Kartonagnick
 // [2025-11-06][06:50:00] 001 Kartonagnick
@@ -9,7 +10,7 @@
 
 #pragma once
 #ifndef dTEST_DEVELOP_USED_ 
-#define dTEST_DEVELOP_USED_ 4
+#define dTEST_DEVELOP_USED_ 5
 
 //=============================================================|||===||=========
 //=== modern/classic/origin ===================================|||===||=========
@@ -25,6 +26,25 @@
 //=============================================================|||===||=========
 //=== compile/info ============================================|||===||=========
     #define TEST_COMPILE_X64_X86                               002   // ready
+//=============================================================|||===||=========
+//=== compile/support =========================================|||===||=========
+    #define TEST_SUPPORT_NULLPTR                               002   // ready
+    #define TEST_SUPPORT_TYPE_TRAITS                           002   // ready
+    #define TEST_SUPPORT_DECLTYPE                              001   // ready
+    #define TEST_SUPPORT_CSTDINT                               001   // ready
+    #define TEST_SUPPORT_LAMBDA                                001   // ready
+    #define TEST_SUPPORT_HASH                                  001   // ready
+    #define TEST_SUPPORT_ATOMIC                                001   // ready
+    #define TEST_SUPPORT_NOCOPYABLE                            001   // ready
+    #define TEST_SUPPORT_NOEXCEPT                              001   // ready
+    #define TEST_SUPPORT_STATIC_CHECK                          001   // ready
+    #define TEST_SUPPORT_STATIC_ASSERT                         001   // ready
+    #define TEST_SUPPORT_VARIADIC_TEMPLATE                     001   // ready
+    #define TEST_SUPPORT_ENUM                                  001   // ready
+    #define TEST_SUPPORT_HAS_EMPLACE                           001   // ready
+    #define TEST_SUPPORT_HAS_USING_ALIAS                       001   // ready
+    #define TEST_SUPPORT_TRAIT_CONST                           001   // ready
+    #define TEST_SUPPORT_CHRONO                                001   // ready
 //==============================================================================
 //==============================================================================
 
