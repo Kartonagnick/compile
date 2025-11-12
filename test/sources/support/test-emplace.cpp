bd56da9a6c8d6b8987ece941a3368f9c2aa97fd0
Kartonagnick <Kartonagnick@gmail.com>
2025-11-12 13:09:36 +0300

    add: test/sources/support/test-emplace.cpp (001) PRE

---
+++ test/sources/support/test-emplace.cpp
@@ -0,0 +1,40 @@
+
+// --- Kartonagnick/compile                                   [test-emplace.cpp]
+// [2025-11-12][19:00:00] 001 Kartonagnick PRE
+//   --- CastleOfDreams/compile (frized)                      [test-emplace.cpp]
+//   [2023-03-08][19:00:00] 001 Kartonagnick
+//     --- Kartonagnick/tools-types (closed)                  [test-emplace.cpp]
+//     [2021-02-26][06:07:32] Idrisov Denis R.
+
+#include <mygtest/modern.hpp>
+//==============================================================================
+//==============================================================================
+
+#ifdef TEST_SUPPORT_HAS_EMPLACE
+#include <compile/support.hpp>
+
+#ifdef dHAS_EMPLACE
+
+#define dTEST_COMPONENT compile, support
+#define dTEST_METHOD HAS_EMPLACE
+#define dTEST_TAG tdd
+
+#include <vector>
+#include <map>
+
+TEST_COMPONENT(000) 
+{
+    std::vector<int> vec;
+    vec.emplace_back(1);
+
+    std::map<int, int> map;
+    map.emplace(1, 1);
+}
+
+//==============================================================================
+//==============================================================================
+#endif // dHAS_EMPLACE
+#endif // TEST_SUPPORT_HAS_EMPLACE
+
+
+
