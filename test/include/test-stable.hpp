614d83b84a333618742408033adfd104175c1a3d
Kartonagnick <Kartonagnick@gmail.com>
2025-11-12 16:05:30 +0300

    upd: test/include/test-stable.hpp (005) PRE

--- test/include/test-stable.hpp
+++ test/include/test-stable.hpp
@@ -1,6 +1,7 @@
 
 // --- Kartonagnick/compile                                    [test-stable.hpp]
-// [2025-11-11][04:30:00] 004 Kartonagnick    
+// [2025-11-12][19:00:00] 005 Kartonagnick PRE
+// [2025-11-11][04:30:00] 004 Kartonagnick
 // [2025-11-10][21:10:00] 003 Kartonagnick
 // [2025-11-06][18:10:00] 002 Kartonagnick
 // [2025-11-06][06:50:00] 001 Kartonagnick
@@ -9,7 +10,7 @@
 
 #pragma once
 #ifndef dTEST_STABLE_USED_ 
-#define dTEST_STABLE_USED_ 4
+#define dTEST_STABLE_USED_ 5
 
 //=============================================================|||===||=========
 //=== modern/classic/origin ===================================|||===||=========
@@ -25,6 +26,16 @@
 //=============================================================|||===||=========
 //=== compile/info ============================================|||===||=========
     #define TEST_COMPILE_X64_X86                               002   // ready
+//=============================================================|||===||=========
+//=== compile/support: c++11, mavc2010 ========================|||===||=========
+    #define TEST_SUPPORT_CHAR_TYPE                             001   // ready
+    #define TEST_SUPPORT_CSTDINT                               001   // ready
+    #define TEST_SUPPORT_DECLTYPE                              001   // ready
+    #define TEST_SUPPORT_HASH                                  001   // ready
+    #define TEST_SUPPORT_LAMBDA                                001   // ready
+    #define TEST_SUPPORT_NULLPTR                               001   // ready
+    #define TEST_SUPPORT_STATIC_CHECK                          001   // ready
+    #define TEST_SUPPORT_TYPE_TRAITS                           001   // ready
 //==============================================================================
 //==============================================================================
 
