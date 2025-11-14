527046eff808ef3296c42f8544f2093605359e1b
Kartonagnick <Kartonagnick@gmail.com>
2025-11-14 12:50:32 +0300

    add: test/sources/support/cpp11-msvc2013/test-aggregate_initialize.cpp (001) PRE

---
+++ test/sources/support/cpp11-msvc2013/test-aggregate_initialize.cpp
@@ -0,0 +1,45 @@
+
+// --- Kartonagnick/compile                      [test-aggregate_initialize.cpp]
+// [2025-11-14][19:00:00] 001 Kartonagnick PRE
+//   --- CastleOfDreams/compile (frized)         [test-aggregate_initialize.cpp]
+//   [2023-03-08][19:00:00] 001 Kartonagnick
+//     --- Kartonagnick/tools-types (closed)     [test-aggregate_initialize.cpp]
+//     [2021-03-15][18:27:18] Idrisov Denis R.
+
+#include <mygtest/modern.hpp>
+//==============================================================================
+//==============================================================================
+
+#ifdef TEST_SUPPORT_AGGREGATE_INITIALIZE
+#include <compile/support.hpp>
+#ifdef dHAS_AGGREGATE_INITIALIZE
+
+#define dTEST_COMPONENT compile, support
+#define dTEST_METHOD HAS_AGGREGATE_INITIALIZE
+#define dTEST_TAG tdd
+
+#include <string>
+#include <vector>
+#include <list>
+
+//==============================================================================
+//==============================================================================
+
+TEST_COMPONENT(001)
+{
+    const std::list<std::string> 
+        example = { "aaabbb", "cccddd", "fffggg", "hhhjjj" };
+    ASSERT_TRUE(example.size() == 4);
+}
+
+TEST_COMPONENT(002)
+{
+    const std::vector<std::string> 
+        example = { "aaabbb", "cccddd", "fffggg", "hhhjjj" };
+    ASSERT_TRUE(example.size() == 4);
+}
+
+//==============================================================================
+//==============================================================================
+#endif // dHAS_AGGREGATE_INITIALIZE
+#endif // TEST_SUPPORT_AGGREGATE_INITIALIZE
