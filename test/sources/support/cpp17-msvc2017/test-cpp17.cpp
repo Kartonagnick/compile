31c988bcfe197b74ea0dd85118c2f4b1995e841e
Kartonagnick <Kartonagnick@gmail.com>
2025-11-16 19:00:00 +0300

    version 0.1.1

--- test/sources/support/cpp17-msvc2017/test-cpp17.cpp
+++ test/sources/support/cpp17-msvc2017/test-cpp17.cpp
@@ -1,6 +1,6 @@
 
 // --- Kartonagnick/compile                                     [test-cpp14.cpp]
-// [2025-11-16][19:00:00] 001 Kartonagnick PRE
+// [2025-11-16][19:00:00] 001 Kartonagnick    
 //   --- CastleOfDreams/compile (frized)                        [test-cpp17.cpp]
 //   [2023-03-08][19:00:00] 001 Kartonagnick
 //     --- Kartonagnick/tools-types (closed)                    [test-cpp17.cpp]
